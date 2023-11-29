fd74203ed2b43721ec1dcff46e281cd17501a562
Xingyu-Jiang <113392209+Xingyu-Jiang@users.noreply.github.com>
2023-11-29 12:10:52 -0500

    Optimized the recipes selelection
    Instead of typing the name the use can just select the names
    
    I think we can use this for filter as well
    We can just ask the user if they want to filter out certain ingredients

--- CookingAssistantGUI.py
+++ CookingAssistantGUI.py
@@ -1,19 +1,28 @@
 import tkinter as tk
 from tkinter import messagebox
-from Testing import *
+from RecipeManager import *
 
 
 class CookingAssistant:
     def __init__(self):
         self.recipes = RecipeManager()
 
-    def displayRecipe(self, name):
-        recipe = self.recipes.getRecipeByName(name)
+    def displayRecipe(self, selected_recipe):
+        self.recipes.loadRecipesFromCSV("recipes.csv")
+        recipe = self.recipes.getRecipeByName(selected_recipe.get())
         if recipe:
-            info = f"Recipe Name: {recipe.getName()}\nInstructions: {recipe.getInstructions()}\nImages: {[image.getImagePath() for image in recipe.getImages()]}\nRating: {recipe.getRating()}"
+            info = (f"Recipe Name: {recipe.getName()}\n"
+                    f"Ingredients: {recipe.getIngredients()}"
+                    f"Instructions: {recipe.getInstructions()}\n"
+                    f"Images: {[image.getImagePath() for image in recipe.getImages()]}\n"
+                    f"Rating: {recipe.getRating()}")
             messagebox.showinfo("Recipe Details", info)
         else:
             messagebox.showerror("Error", "Recipe not found.")
+
+    def getRecipeNames(self):
+        self.recipes.loadRecipesFromCSV("recipes.csv")
+        return [recipe.getName() for recipe in self.recipes.recipes]
 
     def filterRecipes(self, keyword):
         filtered_recipes = self.recipes.filterRecipesByKeyword(keyword)
@@ -26,15 +35,20 @@ class CookingAssistant:
 
 class CookingAssistantGUI:
     def __init__(self, master):
+        # Creating an instance of CookingAssistant to handle actions
+        self.cooking_assistant = CookingAssistant()
+
         self.master = master
         self.master.title("Cooking Assistant")
 
         # Creating widgets
-        self.label = tk.Label(master, text="Enter Recipe Name:")
+        self.label = tk.Label(master, text="Select Recipe:")
         self.label.pack()
 
-        self.entry = tk.Entry(master)
-        self.entry.pack()
+        # Dropdown for recipe selection
+        self.selected_recipe = tk.StringVar(master)
+        self.dropdown = tk.OptionMenu(master, self.selected_recipe, *self.get_recipe_names())
+        self.dropdown.pack()
 
         self.display_button = tk.Button(master, text="Display Recipe", command=self.display_recipe)
         self.display_button.pack()
@@ -48,12 +62,11 @@ class CookingAssistantGUI:
         self.filter_button = tk.Button(master, text="Filter Recipes", command=self.filter_recipes)
         self.filter_button.pack()
 
-        # Creating an instance of CookingAssistant to handle actions
-        self.cooking_assistant = CookingAssistant()
-
     def display_recipe(self):
-        recipe_name = self.entry.get()
-        self.cooking_assistant.displayRecipe(recipe_name)
+        self.cooking_assistant.displayRecipe(self.selected_recipe)
+
+    def get_recipe_names(self):
+        return self.cooking_assistant.getRecipeNames()
 
     def filter_recipes(self):
         keyword = self.filter_entry.get()
---
+++ RecipeManager.py
@@ -0,0 +1,98 @@
+import csv
+
+
+class Image:
+    def __init__(self, imagePath):
+        self.imagePath = imagePath
+
+    def getImagePath(self):
+        return self.imagePath
+
+    def setImagePath(self, imagePath):
+        self.imagePath = imagePath
+
+
+class Recipe:
+    def __init__(self, name, ingredients, instructions, images=None, rating=0):
+        self.name = name
+        self.ingredients = ingredients
+        self.instructions = instructions
+        self.images = images if images else []
+        self.rating = rating
+
+    def getName(self):
+        return self.name
+
+    def getIngredients(self):
+        return self.ingredients
+
+    def getInstructions(self):
+        return self.instructions
+
+    def getImages(self):
+        return self.images
+
+    def getRating(self):
+        return self.rating
+
+    def setName(self, name):
+        self.name = name
+
+    def setIngredients(self, ingredients):
+        self.ingredients = ingredients
+
+    def setInstructions(self, instructions):
+        self.instructions = instructions
+
+    def setImages(self, images):
+        self.images = images
+
+    def setRating(self, rating):
+        self.rating = rating
+
+
+class RecipeManager:
+    def __init__(self):
+        self.recipes = []
+
+    def addRecipe(self, recipe):
+        self.recipes.append(recipe)
+
+    def removeRecipe(self, recipe):
+        if recipe in self.recipes:
+            self.recipes.remove(recipe)
+        else:
+            print("Recipe not found.")
+
+    def getRecipeByName(self, name):
+        for recipe in self.recipes:
+            if recipe.getName() == name:
+                return recipe
+        return None
+
+    def filterRecipesByKeyword(self, keyword):
+        filtered_recipes = []
+        for recipe in self.recipes:
+            if keyword.lower() in recipe.getName().lower():
+                filtered_recipes.append(recipe)
+        return filtered_recipes
+
+    def saveRecipesToCSV(self, filename):
+        with open(filename, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            for recipe in self.recipes:
+                writer.writerow([
+                    recipe.getName(),
+                    recipe.getIngredients(),
+                    recipe.getInstructions(),
+                    recipe.getRating(),
+                    ', '.join([image.getImagePath() for image in recipe.getImages()])
+                ])
+
+    def loadRecipesFromCSV(self, filename):
+        with open(filename, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                name, ingredients, instructions, rating, image_paths = row
+                images = [Image(path.strip()) for path in image_paths.split(',')]
+                self.addRecipe(Recipe(name, ingredients, instructions, images, int(rating)))
